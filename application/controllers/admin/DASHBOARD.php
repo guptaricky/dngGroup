57c7674d89be73486e33e7ce9660847a7245cd4b
guptaricky <mayankgupta1785@gmail.com>
2017-11-26 15:09:20 +0530

    s

--- application/controllers/admin/DASHBOARD.php
+++ application/controllers/admin/DASHBOARD.php
@@ -42,18 +42,22 @@ $this->load->library('email');
 	}
 
 	public function users_accounts(){
-	
-		$this->data['users'] = $this->ion_auth->users()->result();
-			foreach ($this->data['users'] as $k => $user)
-			{
-				$this->data['users'][$k]->groups = $this->ion_auth->get_users_groups($user->id)->result();
-			}
-		$this->load->view('default_admin/head');
-		$this->load->view('default_admin/header');
-		$this->load->view('default_admin/sidebar');
-		$this->load->view('admin/DASHBOARD/users_accounts', $this->data);
-		$this->load->view('default_admin/footer');
-		
+		if($this->ion_auth->logged_in())
+		{
+			$this->data['users'] = $this->ion_auth->users()->result();
+				foreach ($this->data['users'] as $k => $user)
+				{
+					$this->data['users'][$k]->groups = $this->ion_auth->get_users_groups($user->id)->result();
+				}
+			$this->load->view('default_admin/head');
+			$this->load->view('default_admin/header');
+			$this->load->view('default_admin/sidebar');
+			$this->load->view('admin/DASHBOARD/users_accounts', $this->data);
+			$this->load->view('default_admin/footer');
+		}
+		else{
+			redirect('auth/login');	
+		}
 	}
 
 	
